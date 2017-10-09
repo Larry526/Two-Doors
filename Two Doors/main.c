0708c9ed5a9b6af5e36fc4cfc378aabef04e8f41
Larry Luk <larryluk526@gmail.com>
2017-10-09 18:03:33 -0400

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by Larry Luk on 2017-10-09.
+//  Copyright © 2017 App App Studio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
